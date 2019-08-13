4c9be827e8b40e1a115cc414a28d02dc70de3b4d
huang <tianzaihuang@gmail.com>
2019-08-13 15:56:37 +0800

    偷偷把之前忘了写的main方法补上,再偷偷删了那些稀奇古怪的log

--- main.go
+++ main.go
@@ -2,28 +2,9 @@ package main
 
 import (
 	"./GUI"
-	"fmt"
-	"flag"
-	"log"
 )
 
 func main() {
-	testReadFile()
-
+	GUI.CMD()
 }
-func testReadFile() {
-	str := flag.String("conf", "", "配置文件的路径,文件正文以config开头的那个")
-	ip := flag.String("ip", "0.0.0.0", "配置文件里设置的ip地址暂时是摆设,如果要指定IP的话还要在设一遍")
-	flag.Parse()
-	if *str == "" {
-		log.Fatal("再懒也不能懒到配置文件也不传入啊")
-	}
-	rules, err := GUI.ReadConfigFile(*str)
-	if err != nil {
-		fmt.Println(err)
-	}
-	listen := GUI.NewEasyListenToPortRuleList(*ip, rules...)
-	listen.Listen()
-	listen.Commit()
 
-}
